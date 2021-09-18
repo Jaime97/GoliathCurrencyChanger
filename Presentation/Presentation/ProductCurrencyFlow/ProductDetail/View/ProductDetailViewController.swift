8a4f2cb82ac888d56e2ebd1025960f40286d049a
Jaime Alcántara Arnela <jaime061197@gmail.com>
2021-09-18 16:51:56 +0200

    Design initial detail interface

--- Presentation/Presentation/ProductCurrencyFlow/ProductDetail/View/ProductDetailViewController.swift
+++ Presentation/Presentation/ProductCurrencyFlow/ProductDetail/View/ProductDetailViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ProductDetailViewController: UIViewController {
     
+    @IBOutlet weak var productCodeLabel: UILabel!
+    
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBOutlet weak var transactionTableView: UITableView!
+    
     var presenter: ProductDetailPresenterProtocol!
 
     override func viewDidLoad() {
